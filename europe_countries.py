41ef828d2b15f50210b70185c90226bd35fb3501
Mathias <meszaros.mathias@gmail.com>
2025-02-23 14:43:33 +0100

    final

---
+++ europe_countries.py
@@ -0,0 +1,11 @@
+import streamlit as st
+from sqlalchemy import create_engine
+import pandas as pd
+
+engine = create_engine('mysql+mysqlconnector://root:Matthew19961024@localhost/europe')
+
+query = "SELECT * FROM countries"
+df = pd.read_sql(query, engine)
+
+st.title('Europe Countries Languages & Population')
+st.dataframe(df)
